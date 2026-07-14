ce28ba60cdf015fabacabfc85cbf3da46d4e4e43
Önder <eduhamoen001@fh-kaernten.ac.at>
2026-07-14 11:47:14 +0200

    minor fix

--- start_motor_driver.sh
+++ start_motor_driver.sh
@@ -3,5 +3,4 @@
 # Source the standard ROS2 environment (Change 'humble' if you use another version)
 source /opt/ros/humble/setup.bash
 
-# Execute the node with the desired interval argument
 python3 /home/services/motor_driver.py
